88140c91bbfca5f6e8686a8fb20c93d795369885
piotr-kalanski <piotr.kalanski@gmail.com>
2021-02-06 11:48:56 +0100

    Remove not needed fragments

--- app/src/main/java/com/github/piotrkalanski/ui/cameras/MyCameraRecyclerViewAdapter.java
+++ app/src/main/java/com/github/piotrkalanski/ui/cameras/MyCameraRecyclerViewAdapter.java
@@ -27,7 +27,7 @@ public class MyCameraRecyclerViewAdapter extends RecyclerView.Adapter<MyCameraRe
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.fragment_cameras, parent, false);
+                .inflate(R.layout.fragment_camera_card, parent, false);
         return new ViewHolder(view);
     }
 
